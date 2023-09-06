4d4ed93464222cb7a7036f2212deb82956702496
oliFYP <k2026944@kingston.ac.uk>
2023-09-06 18:18:16 +0100

    link changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route index element={<Home/>}/>
-   
+        <Route path="/ao-fitness" element={<Home/>}/>
       </Routes>
       </BrowserRouter>
     </div>
