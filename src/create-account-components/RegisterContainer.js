03f71da718ea5086c11c7c4a0b5c20ec5ea2a920
oliFYP <k2026944@kingston.ac.uk>
2023-09-22 17:24:47 +0100

    margin modifications register container

--- src/create-account-components/RegisterContainer.js
+++ src/create-account-components/RegisterContainer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function RegisterContainer() {
   return (
     <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-    <div className="max-w-md w-full mr-5 ml-5 p-6 bg-black bg-opacity-50 backdrop-blur-lg rounded-lg grid grid-cols-2 gap-4">
+    <div className="max-w-md w-full mr-2 ml-2 p-6 bg-black bg-opacity-50 backdrop-blur-lg rounded-lg grid grid-cols-2 gap-4">
       <div className="col-span-2 mb-4">
         <h2 className="text-2xl font-bold text-white">Register</h2>
       </div>
